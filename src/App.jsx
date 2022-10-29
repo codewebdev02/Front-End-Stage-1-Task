e00b02f37438782ab031a36301ce3962c67871ed
webdev0202 <webdev0202@yandex.com>
2022-10-29 09:58:55 +0100

    books a tag button commit

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ function App() {
       </div>
       <div className="link-section">
         <a id="btn__zuri" href="https://training.zuri.team/">Zuri Team </a>
-        <a id="books" href="https://books.zuri.team/">Zuri Books</a>
+        <a id="books" href="https://books.zuri.team/">Zuri Books </a>
         <a id="book__python" href="https://books.zuri.team/python-for-beginners?ref_id=Eaxysunshine">Python Books</a>
         <a id="pitch" href="https://background.zuri.team/">Background Check for Coders</a>
         <a id="book__design" href="https://books.zuri.team/design-rules">Design Books</a>
