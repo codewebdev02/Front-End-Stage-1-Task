e367dc4e550679d74c3f6f00a6f9b0f4b2abb627
webdev0202 <webdev0202@yandex.com>
2022-10-29 08:41:29 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container AppHeader">
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
